b5f5f6d2a7e94e5893e12961077468e25b7f8f1c
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-05-16 10:30:37 +0300

    tshw2

--- src/apiService/getImages.ts
+++ src/apiService/getImages.ts
@@ -1,13 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = "KHY_qjzNhH1UiRyRmWjLVIL4iaIIKfBINoOo9m-Q8J4";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 axios.defaults.params = {
-    orientation: "landscape",
-    per_page: 12,
+  orientation: "landscape",
+  per_page: 12,
 };
 
 export const getImages = async <T>(query: string, page: number): Promise<T> => {
-    const { data } = await axios.get<T>(`search/photos?client_id=${API_KEY}&query=${query}&page=${page}`);
-    return data;
-}
+  const { data } = await axios.get<T>(
+    `search/photos?client_id=${API_KEY}&query=${query}&page=${page}`
+  );
+  return data;
+};
